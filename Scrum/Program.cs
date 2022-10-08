ed545e3d52a2be40b999bb7e8fe40107a907a8d8
Julien <julienrousselle@outlook.be>
2022-10-08 16:14:31 +0200

    Add project files.

---
+++ Scrum/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using var answers = new StreamReader(File.OpenRead("Answers.txt"));
+using var questions = new StreamReader(File.OpenRead("Questions.txt"));
+
+var questionCounts = questions.ReadToEnd().Split("###", StringSplitOptions.RemoveEmptyEntries).Count();
+questions.BaseStream.Seek(0, SeekOrigin.Begin);
+
+var question = questions.ReadLine();
+var anwser = answers.ReadLine();
+
+int currentIndex = 0;
+
+while (true)
+{
+    Console.WriteLine($"Question {++currentIndex} of {questionCounts}");
+    Console.WriteLine();
+    Console.WriteLine();
+
+    do
+    {
+        Console.WriteLine(question);
+        question = questions.ReadLine();
+    } while (!question.StartsWith("###"));
+
+    Console.ReadKey();
+
+    do
+    {
+        Console.WriteLine(anwser);
+        anwser = answers.ReadLine();
+    } while (!anwser.StartsWith("###"));
+
+    Console.ReadKey();
+    Console.Clear();
+}
