60e673288150ffb99f772cc3dbf8e0178e3c6990
Julien <julienrousselle@outlook.be>
2022-10-15 16:07:33 +0200

    wip blazor scrum app

---
+++ BlazorApp.Scrum/Data/Question.cs
@@ -0,0 +1,18 @@
+namespace BlazorApp1.Data
+{
+    public class Question
+    {
+        public int Index { get; set; }
+        public string Text { get; set; }
+
+        public Answer[] Answers { get; set; }
+    }
+
+    public class Answer
+    {
+        public int Index { get; set; }
+        public string Text { get; set; }
+        public bool IsSelected { get; set; }
+        public bool IsUserSelected { get; set; }
+    }
+}
---
+++ BlazorApp.Scrum/Data/QuestionService.cs
@@ -0,0 +1,35 @@
+namespace BlazorApp1.Data
+{
+    public class QuestionService
+    {
+        public async Task<Question> GetQuestion(int index)
+        {
+            using var questionsStream = new StreamReader(File.OpenRead("Resources/Questions.txt"));
+
+            var questionDetails = (await questionsStream.ReadToEndAsync()).Split("###", StringSplitOptions.RemoveEmptyEntries);
+
+            var questionDetailSerialized = questionDetails[index];
+
+            var questionContent = questionDetailSerialized.Split("\r\n", StringSplitOptions.RemoveEmptyEntries);
+
+            return new Question()
+            {
+                Index = index,
+                Text = questionContent[0],
+                Answers = BuildAnswer(questionContent.Skip(1).ToList())
+            };
+        }
+
+        private Answer[] BuildAnswer(List<string> answerList)
+        {
+            return answerList.Select(a =>
+                new Answer()
+                {
+                    Index = answerList.IndexOf(a),
+                    Text = a.Replace("- [ ] ", "").Replace("- [x] ", ""),
+                    IsSelected = a.StartsWith("- [x] ")
+                }
+            ).ToArray();
+        }
+    }
+}
