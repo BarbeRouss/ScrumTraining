30a0172670381697761ac088970155e035e4a069
Julien <julienrousselle@outlook.be>
2022-10-15 16:38:00 +0200

    http only

--- BlazorApp.Scrum/Data/Question.cs
+++ BlazorApp.Scrum/Data/Question.cs
@@ -1,4 +1,4 @@
-namespace BlazorApp1.Data
+namespace Blazor.Scrum.Data
 {
     public class Question
     {
--- BlazorApp.Scrum/Program.cs
+++ BlazorApp.Scrum/Program.cs
@@ -1,4 +1,4 @@
-using BlazorApp1.Data;
+using Blazor.Scrum.Data;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 
@@ -18,8 +18,6 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
-app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 
